3412cac4b3f3b39a2fad5a0e3b70cdb58ac6c829
Lazzaretti Luca <76959613+lazza95@users.noreply.github.com>
2022-06-07 17:02:01 +0200

    Add files via upload

---
+++ compito 0706/index.js
@@ -0,0 +1,47 @@
+let nome = "Mirtilla";
+var stato = "Cliente";
+const  piGreco = 3.14;   
+
+console.log(nome);
+console.log(stato);
+console.log(piGreco);
+
+nome = "Luca";
+stato = "Developer";
+
+console.log(nome);
+console.log(stato);
+
+let formazione01 = "Developer";
+let formazione02 = "Formazione";
+
+console.log("Io mi occupo di: " +formazione01+ " e " +formazione02);
+
+let professione01 =  formazione01+ " e " +formazione02;
+console.log("Io mi occupo di:  " + professione01);
+
+let js = true;
+console.log("Let JS:" + js);
+
+let nondefinito;
+console.log(nondefinito);
+
+let anni = 30;
+console.log(anni)
+console.log("Anna ha " + anni +" anni ");
+
+let anni2 = 15;
+console.log(anni - 15);
+console.log(anni - 4);
+console.log(anni- 1);
+
+let nomeAnimale = "George";
+let nomeColore = "Giallo";
+console.log("Il nome del mio pet: " +nomeAnimale+ " il suo colore è: " +nomeColore);
+
+let nomeAnimale01 = "Billie";
+console.log("Il nome del mio per: "  +nomeAnimale01+  " il suo colore è: " +nomeColore);
+
+console.log("string\ncon righe\nmultiple");
+console.log("string\ncon righe\nmultiple");
+
